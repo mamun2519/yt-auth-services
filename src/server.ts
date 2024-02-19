ff30e5224d86c34c1dc475ed83f6f6fc6e107cee
mamunMonster <mamun19b3@gmail.com>
2024-02-19 17:06:17 +0600

    complete setup or database connect

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,17 @@
 import app from './app'
 import config from './config'
-
+import mongoose from 'mongoose'
 // Main Server
-const bootstrap = () => {
-  app.listen(config.port, () => {
-    console.log(`server Is Run Successfully PORT NO- ${config.port}`)
-  })
+const bootstrap = async () => {
+  try {
+    await mongoose.connect(config.database_url as string)
+    console.log('database connect')
+    app.listen(config.port, () => {
+      console.log(`server Is Run Successfully PORT NO- ${config.port}`)
+    })
+  } catch (error) {
+    console.log(error)
+  }
 }
 
 bootstrap()
