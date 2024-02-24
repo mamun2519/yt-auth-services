296a81c35b23c341e728626bbe0231779f74b7d1
mamunMonster <mamun19b3@gmail.com>
2024-02-24 14:30:08 +0600

    add validation feedback api

--- src/app/modules/feedback/feedback.route.ts
+++ src/app/modules/feedback/feedback.route.ts
@@ -1,11 +1,17 @@
 import express from 'express'
 import { FeedbackController } from './feedback.controller'
+import ValidateRequestAPI from '../../middleware/ValidationRequest'
+import { FeedbackValidation } from './feedback.validation'
 
 const router = express.Router()
 router.get('/', FeedbackController.insertFeedback)
 router.get('/:id', FeedbackController.getFeedBackById)
 router.delete('/:id', FeedbackController.deleteFeedback)
 router.patch('/:id', FeedbackController.updateFeedbackById)
-router.post('/', FeedbackController.insertFeedback)
+router.post(
+  '/',
+  ValidateRequestAPI(FeedbackValidation.FeedbackPostZodSchema),
+  FeedbackController.insertFeedback,
+)
 
 export const FeedbackRoute = router
---
+++ src/app/modules/feedback/feedback.validation.ts
@@ -0,0 +1,19 @@
+import { z } from 'zod'
+
+const FeedbackPostZodSchema = z.object({
+  body: z.object({
+    reason: z.string({
+      required_error: 'Reason  is required',
+    }),
+    email: z.string({
+      required_error: 'Email  is required',
+    }),
+    message: z.string({
+      required_error: 'Message  is required',
+    }),
+  }),
+})
+
+export const FeedbackValidation = {
+  FeedbackPostZodSchema,
+}
